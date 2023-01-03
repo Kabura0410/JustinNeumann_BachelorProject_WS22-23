fc447383f9184a16cdaa4a23b18d8881af594b9a
Marc749 <marc.wandrei@freenet.de>
2023-01-03 14:17:27 +0100

    portal spin implemented

--- BA-2022-23/Assets/Scripts/Portal.cs
+++ BA-2022-23/Assets/Scripts/Portal.cs
@@ -20,9 +20,16 @@ public class Portal : MonoBehaviour, IInteractable
     [SerializeField] private CameraType cameraType;
 
     public bool PlayerInTrigger { get => playerInTrigger; set => playerInTrigger = value; }
-    public bool CanInteract { get => true; set => CanInteract = true; }
+    public bool CanInteract { get => canInteract; set => canInteract = value; }
 
     private bool playerInTrigger;
+
+    private bool canInteract;
+
+    private void Start()
+    {
+        canInteract = true;
+    }
 
     private void Update()
     {
@@ -39,10 +46,12 @@ public class Portal : MonoBehaviour, IInteractable
 
     private IEnumerator TeleportPlayerDelayed()
     {
+        GameManager.instance.player.animPortal.SetTrigger("doPortalSpin");
         GameManager.instance.player.ToggleMovement();
         GameManager.instance.player.rb.gravityScale = 0;
         GameManager.instance.player.rb.velocity = Vector2.zero;
         movePlayer = true;
+        canInteract = false;
         yield return new WaitForSecondsRealtime(portDelay);
         switch (cameraType)
         {
@@ -54,17 +63,20 @@ public class Portal : MonoBehaviour, IInteractable
                 GameManager.instance.inShop = false;
                 ShopManager.instance.DeleteAllObsoleteObjects();
                 ShopManager.instance.ToggleShopSlotCanvases();
+                GameManager.instance.player.animPortal.SetTrigger("stopPortalSpin");
                 break;
             case CameraType.shopCam:
                 GameManager.instance.ChangeToShopCam();
                 GameManager.instance.inShop = true;
                 ShopManager.instance.InstantiateShop();
                 ShopManager.instance.ToggleShopSlotCanvases();
+                GameManager.instance.player.animPortal.SetTrigger("stopPortalSpin");
                 break;
         }
         GameManager.instance.player.transform.position = portPosition.position;
         GameManager.instance.player.ToggleMovement();
         GameManager.instance.player.rb.gravityScale = GameManager.instance.player.StartGravity;
+        canInteract = true;
         movePlayer = false;
     }
 
